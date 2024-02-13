47027acbc887451c4175987f228b1e2c1b681f58
himzei <himzei@gmail.com>
2024-02-13 10:59:52 +0900

    ci/cd

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello nodejs github push");
+  res.send("hello nodejs github push addsafjlsdajfl");
 });
 
 app.listen(PORT, () => console.log(`💛Sever on: http://localhost:${PORT}`));
