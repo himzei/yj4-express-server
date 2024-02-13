ef3063b862426b576bc558962ef0153008bc3208
himzei <himzei@gmail.com>
2024-02-13 10:40:26 +0900

    add file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello nodejs");
+  res.send("hello nodejs github push");
 });
 
 app.listen(PORT, () => console.log(`💛Sever on: http://localhost:${PORT}`));
